945c4c7766fc3e895794463ac89c2c3c03924e5b
nico <nicolaspereyra096@gmail.com>
2025-08-08 22:31:02 -0300

    primer commit del proyecto

---
+++ tp3_consignas.py
@@ -0,0 +1,24 @@
+#consigna
+##objetivos
+
+##requisitos
+link de github con el proyecto totalmente subido a la plataforma
+
+proyecto web django con patron mvt que incluya:
+
+herencia  templates
+por lo menos 1 clases en models
+un formulario para insertar datos a por cada model creado
+una vista que contenga el listado de los objetos del modelo
+barra de busqueda en el listado
+readme que indique el orden en el que se prueban las cosas y/o donde estan las funcionalidades
+
+**IMPORTANTE**: a los puntos anteriores se le suma toda la estructura de archivos como lo vimos en clase
+.gitignore
+requirements.txt
+la navegacion
+etc
+
+##formato
+
+link al repositorio de github con el nombre "tuprimerapagina+apellido" por ejemplo "tuprimerapagina+fernandez"
